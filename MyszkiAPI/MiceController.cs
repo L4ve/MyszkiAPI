ded9bd9fd7fd89a4fce32b6f21f403bbc40f66e9
L4ve <pawelskowronski2008@gmail.com>
2026-01-11 23:01:30 +0100

    optymalizacja

--- MyszkiAPI/MiceController.cs
+++ MyszkiAPI/MiceController.cs
@@ -6,23 +6,22 @@ namespace MyszkiAPI.Controllers
     [Route("api/[controller]")]
     public class MiceController : ControllerBase
     {
-        //zwraca wszysztkie myszki
-        [HttpGet] public IEnumerable<Mouse1> GetAll() => MouseRepository.GetAll();
-        // zwraca 1 myszke po id
-        [HttpGet("{id}")]
-        public ActionResult<Mouse1> GetById(int id) =>
-            MouseRepository.GetById(id) is Mouse1 mouse ? mouse : NotFound();
-        // dodaje nową myszke
+        [HttpGet]
+        public ActionResult Get([FromQuery] int? id)
+        {
+            if (id.HasValue)
+            {
+                var mouse = MouseRepository.GetById(id.Value);
+                return mouse is not null ? Ok(mouse) : NotFound();
+            }
+
+            return Ok(MouseRepository.GetAll());
+        }
         [HttpPost]
-        public ActionResult<Mouse1> Post(Mouse1 mouse) =>
-            CreatedAtAction(nameof(GetById), new { id = MouseRepository.Add(mouse).Id }, mouse);
-        // aktualizuje nową myszke
-        [HttpPut("{id}")]
-        public ActionResult<Mouse1> Put(int id, Mouse1 mouse) =>
-            MouseRepository.Update(id, mouse) is Mouse1 updated ? updated : NotFound();
-        // usuwa nową myszke
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id) =>
-            MouseRepository.Delete(id) ? NoContent() : NotFound();
+        public ActionResult<Mouse1> Post(Mouse1 mouse)
+        {
+            var addedMouse = MouseRepository.Add(mouse);
+            return CreatedAtAction(nameof(Get), new { id = addedMouse.Id }, addedMouse);
+        }
     }
 }
